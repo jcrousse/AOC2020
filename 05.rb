6b131597893e05fa08a09b89afe7aac8e9c50f67
John Crousse <jcrousse@gmail.com>
2020-12-05 20:37:00 +0100

    finalised day 5

--- 05.rb
+++ 05.rb
@@ -4,9 +4,7 @@ seats_input =  file.readlines
 def binarypartition_to_seatid(partition)
   row_val = partition[0..7].gsub('F', '0').gsub('B', '1').to_i(2)
   col_val = partition[7..].gsub('L', '0').gsub('R', '1').to_i(2)
-  seat_id = row_val * 8 + col_val
-  puts seat_id
-  seat_id
+  row_val * 8 + col_val
 end
 
 puts binarypartition_to_seatid("BFFFBBFRRR")
@@ -15,4 +13,13 @@ puts binarypartition_to_seatid("BBFFBBFRLL")
 
 seat_ids = seats_input.map{|val| binarypartition_to_seatid(val)}
 
-puts seat_ids.max
+puts "Highest seat number : #{seat_ids.max}"
+
+seat_ids = seat_ids.sort!
+seat_ids.each_index do |idx|
+  if idx.between?(1, seat_ids.length - 2)
+    unless (seat_ids[idx -1] + seat_ids[idx + 1]) / 2 == seat_ids[idx]
+      puts "Your seat number: #{seat_ids[idx] - 1}"
+    end
+  end
+end
